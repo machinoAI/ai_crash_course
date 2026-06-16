c7a7c4ca8725cae00c83d1e2c9c8a2490a945cbe
machinoAI <ravikk87@gmail.com>
2026-06-17 00:06:11 +0530

    using numpy cosine similarity

--- python/deep_learning/cosine_similarity.py
+++ python/deep_learning/cosine_similarity.py
@@ -18,3 +18,16 @@ y = [5,6,7]
 
 cosine_sim = cosine_similarity(x,y)
 print(cosine_sim)
+
+# Numpy:
+
+import numpy as np
+
+def cosine_similarity(vec1, vec2):
+
+    return np.dot(vec1, vec2) / (
+        np.linalg.norm(vec1) * np.linalg.norm(vec2)
+    )
+
+cosine_similarity(x,y)
+print("Using Numpy:", cosine_sim)
