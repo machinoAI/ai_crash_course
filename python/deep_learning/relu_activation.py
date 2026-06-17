22a22ec1a72ea37721da7316f062a5d7314bff8e
machinoAI <ravikk87@gmail.com>
2026-06-17 14:01:49 +0530

    ReLU activation function

---
+++ python/deep_learning/relu_activation.py
@@ -0,0 +1,22 @@
+# ReLU (Rectified Linear Unit) is an activation function that keeps positive values and converts negative values to zero.
+# ReLU(x)=max(0,x)  If x>0 → output = x  If x≤0 → output = 0
+
+
+import torch
+from torch.ao.nn.quantized.functional import leaky_relu
+
+x = torch.tensor([3.0, -2.0, 5., 7., -9.0])
+y = torch.relu(x)
+
+
+print("ReLU:", y)
+
+
+
+#Leaky ReLU: is a variant of ReLU that allows a small negative output instead of making all negative values zero. This prevents neurons from becoming permanently inactive.
+# f(x) = { x if x> 0 , αx if x<=0, where α = small constant (typically 0.01)
+import torch.nn as nn
+
+leaky_relu1 = nn.LeakyReLU(negative_slope=0.01)
+
+print("Leaky ReLU:", leaky_relu1(x))
