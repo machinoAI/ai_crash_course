39116d968959a52f7f52f0d610b2ced10e513f2c
machinoAI <ravikk87@gmail.com>
2026-06-26 14:59:27 +0530

    optimized

--- python/dsa/arrays/most_water_container.py
+++ python/dsa/arrays/most_water_container.py
@@ -17,11 +17,12 @@ height = [1,8,6,2,5,4,8,3,7]
 
 # output: 49
 
+# Brute force: O(n) & O(1)
 def most_water_container(height):
     max_area =0
 
     for i in range(len(height)):
-        for j in range(i, len(height)):
+        for j in range(i+1, len(height)):
             width = j - i
             min_height=min(height[i], height[j])
             area = width*min_height
@@ -30,4 +31,29 @@ def most_water_container(height):
     return  max_area
 
 
-print(most_water_container(height))
+print("Max Area: ", most_water_container(height))
+
+
+# Optimized solution:
+height = [1,8,6,2,5,4,8,3,7]
+
+def most_water_container(height):
+    left = 0
+    right = len(height)-1
+    max_area= 0
+
+    while left < right:
+        width = right-left
+        min_height = min(height[left], height[right])
+        area = width * min_height
+
+        max_area = max(max_area, area)
+
+        if height[left] < height[right]:
+            left +=1
+        else:
+            right -=1
+
+    return  max_area
+
+print("Optimised solution- Max Area: ", most_water_container(height))
