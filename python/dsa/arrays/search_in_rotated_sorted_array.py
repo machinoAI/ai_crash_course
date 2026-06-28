4747a9d6b744f35215cdeecbc162d66f9585ea5d
machinoAI <ravikk87@gmail.com>
2026-06-28 12:18:56 +0530

    rotated sorted array

---
+++ python/dsa/arrays/search_in_rotated_sorted_array.py
@@ -0,0 +1,55 @@
+"""
+You are given a sorted array that has been rotated at some pivot.
+Given the rotated array and a target value, return the index of the target.
+
+If the target is not found, return -1.
+
+You must solve it in O(log n) time.
+"""
+
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+
+def rotated_array(nums, target):
+    index = -1
+    for i, num in enumerate(nums):
+        if target == num:
+            index = i
+
+    return index
+
+
+print(rotated_array(nums, target))
+
+
+#Binary Search to achieve O(log n)
+
+def search_rotated(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        # Left half is sorted
+        if nums[left] <= nums[mid]:
+
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        # Right half is sorted
+        else:
+
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
