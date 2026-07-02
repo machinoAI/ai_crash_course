eda42b532d570854aff8dc331e94648f0af0e563
machinoAI <ravikk87@gmail.com>
2026-07-02 15:27:42 +0530

    basics of deep learning

---
+++ python/deep_learning/Basics.py
@@ -0,0 +1,18 @@
+"""
+1. Epoch: One complete pass of the entire dataset through the model
+
+ 2. Batch: A subset of training data processed together
+    The training data split into smaller groups called batches.
+    The model processed one batch a time
+
+3. Batch size: Number of training data into one batch.
+
+Example: Lets training sample is 10. batchsize = 2
+ So # of batches = 10/2 = 5 batch will get created , with 2 samples each.
+
+ 4. Iteration: Processing one batch of data through the model...to process 5 batches it will take 5 iteration.
+
+ 5.
+
+
+"""
