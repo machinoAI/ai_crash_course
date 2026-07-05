7dd8a4235dc3b3fbfb16bb5e1edc6cbb787ab953
machinoAI <ravikk87@gmail.com>
2026-07-05 08:53:00 +0530

    substring_palindrom.py

--- python/dsa/strings/substring_palindrom.py
+++ python/dsa/strings/substring_palindrom.py
@@ -2,18 +2,23 @@ def SearchingChallenge(strParam):
     start = 0
     end = 0
 
+    # Starting from middle and stretching like rubber:
     def expand(left, right):
         while left >= 0 and right < len(strParam) and strParam[left] == strParam[right]:
-            left -= 1
-            right += 1
+            left -= 1             # Going towards left till -1
+            right += 1            # Going towards, right till end
 
-        return left + 1, right - 1
+        return left + 1, right - 1   #string between these are palindrome
 
     for i in range(len(strParam)):
+
+        # Odd palindrome
         l1, r1 = expand(i, i)
 
+        # Even palindrome
         l2, r2 = expand(i, i + 1)
 
+        # comparing the index
         if r1 - l1 > end - start:
             start, end = l1, r1
 
