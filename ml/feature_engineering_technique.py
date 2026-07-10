0aff192c6ad6f4dc8b35833aec6bbff750383f81
machinoAI <ravikk87@gmail.com>
2026-07-10 09:13:56 +0530

    3 coins

---
+++ ml/feature_engineering_technique.py
@@ -0,0 +1,10 @@
+"""
+Common Feature Engineering Techniques:
+ - Handling missing values.
+ - Encoding categorical variables (one-hot, label encoding).
+ - Feature scaling (normalization, standardization).
+ - Binning/bucketing.
+ - Log transformation.
+ - Creating interaction features.
+ - Feature selection
+"""
---
+++ probability/three_coins.py
@@ -0,0 +1,7 @@
+"""
+Flip three fair coins. Given that at least one is heads, what is the probability of exactly one head?
+Explanation:
+    [T,T,T] doesn't have any head hence total outcomes to get at least one head = 7  not 8
+     = 3/7
+
+"""
