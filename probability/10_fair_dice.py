75a31e3c848ac55d87375eaf28ecf5c3d96cb8b6
machinoAI <ravikk87@gmail.com>
2026-07-10 07:41:00 +0530

    10 dice

---
+++ probability/10_fair_dice.py
@@ -0,0 +1,11 @@
+"""
+Suppose you have ten fair dice. If you randomly throw them simultaneously, what is the probability that the sum of all the top faces is divisible by six?
+
+Explanation:
+
+- The sum of first 9 dice faces is S. And 10th face is d10
+- (S+d10 ) mod6 = 10
+
+- So No matter what is the sun of S, it will always depende on the d10 ( 1,2,3,4,5,6) to get it divisible by 6.
+- so the probability is 1/6.
+"""
