9db0a12c9af33e069032960939a395a5cef54db2
machinoAI <ravikk87@gmail.com>
2026-07-10 07:47:37 +0530

    fixed

---
+++ probability/cards.py
@@ -0,0 +1,11 @@
+"""
+Assume you have a deck of 100 cards with values ranging from 1 to 100.
+If you draw two cards randomly without replacement, what is the probability that the number of one of them is double the other
+
+Explanation:
+    Total ways to choose two cards from 100:
+        = 100*99/2 = 4950
+    There are 50 favourable pairs where number of one of them is double:
+        (1,2), (2,4) = 50/4950 = 1/99
+
+"""
