006dec39e0e2305d892d054b5d9fa4ad44634ea5
machinoAI <ravikk87@gmail.com>
2026-07-10 08:59:15 +0530

    eigenvectors

---
+++ stats/eigenvectors.py
@@ -0,0 +1,17 @@
+"""
+An eigenvector is a vector whose direction remains unchanged after applying a matrix transformation.
+
+Mathematically, Av=λv
+
+    where:
+        A = matrix
+        v = eigenvector
+        λ = eigenvalue
+
+    Why do we use them in ML?
+    - PCA (find directions with maximum variance).
+    - Dimensionality reduction.
+    - Spectral clustering.
+    - Recommendation systems.
+    - Google's PageRank.
+"""
