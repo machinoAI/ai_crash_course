f57778f058bfb6328d507ad68d29ac3eea0339d0
machinoAI <ravikk87@gmail.com>
2026-07-12 04:28:30 +0530

    convex?

---
+++ ml/linear_regression/convexity.py
@@ -0,0 +1,14 @@
+"""
+What is meaning of convex ?
+    - A function is convex if it has a single bowl shape, i.e., no local minima
+
+Is MSE convex?
+    - Yes
+Is MAE convex?
+    - Yes
+Why MAE is harder to optimize event it is convex ?
+    - Because it is not differentiable .
+
+
+
+"""
