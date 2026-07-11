c602d088f3e81b680181aa4a9b6c6179ae477afe
machinoAI <ravikk87@gmail.com>
2026-07-12 04:39:10 +0530

    LR assumptions

---
+++ ml/linear_regression/assumptions_of_LR.py
@@ -0,0 +1,15 @@
+"""
+Assumptions of Linear Regression:
+    - Linearity -  Most important
+        y=wX+b+ϵ
+
+    - Independence : Observations should be independent of each other.
+    - Homoscedasticity : Residuals should have constant variance:
+        Var(ϵ)=σ2
+
+    - No multi-collinearity: Features should not be highly correlated.
+    - Normality of residuals:
+        ϵ∼N(0,σ2)
+
+
+"""
