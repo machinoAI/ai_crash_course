8e24277d66a1483ec9af6284dcf0c55c78cea8bf
machinoAI <ravikk87@gmail.com>
2026-07-11 16:07:36 +0530

    fixed

--- ai/agents/Agent_evaluations.py
+++ ai/agents/Agent_evaluations.py
@@ -8,7 +8,7 @@ AI Evaluation Metrics: Put into two buckets
 2. Technical metrics:
   - Tool correctness:           How exactly agent was able to predict the tool.
   - Tool argument accuracy:     How many parameters were correct including their values.
-  - Agent comes with an answer instead of tool call. Such cases need to compute answer correctness using semantic similarity and LLM-as judge.
-  - Measure the reasoning quality by measuring the plan quality and plan adherence.
+  - LLM-as-judge: Agent comes with an answer instead of tool call. Such cases need to compute answer correctness using semantic similarity and LLM-as judge.
+  - Measure the reasoning quality:  by measuring the plan quality and plan adherence.
 
 """
