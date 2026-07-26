1fd4aaa6f56ea965e7420638ef15502e7c537d66
machinoAI <ravikk87@gmail.com>
2026-07-26 23:53:51 +0530

    peak value

--- dsa/binary_search/peak_element.py
+++ dsa/binary_search/peak_element.py
@@ -27,7 +27,25 @@ def peak_element(nums):
 
 
 nums = [1,2,3,4,5,6,7,8,5,1]
-nums = [1,2,1,3,5,6,4]
-nums = [5, 4, 3, 2, 1]
+# nums = [1,2,1,3,5,6,4]
+# nums = [5, 4, 3, 2, 1]
+
+print(peak_element(nums))
+
+def peak_element(nums):
+
+    low = 0
+    high = len(nums)-1
+
+    while low < high:
+
+        mid = (high+low)//2
+
+        if nums[mid] > nums[mid+1]:
+            high = mid
+        else:
+            low = mid+1
+
+    return low
 
 print(peak_element(nums))
