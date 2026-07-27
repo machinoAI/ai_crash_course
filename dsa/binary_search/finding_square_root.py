268e3da12ba680281b1d62e18210b00b9ed0f997
machinoAI <ravikk87@gmail.com>
2026-07-27 09:21:58 +0530

    finding square root

--- dsa/binary_search/finding_square_root.py
+++ dsa/binary_search/finding_square_root.py
@@ -12,11 +12,10 @@ Output: 5
 Explanation: Square root of 28 is approximately 5.292. So, the floor value will be 5.
 """
 
-
 def find_square_root(num):
 
 
-    for i in range(1, num):
+    for i in range(1, num+1):
         square = i*i
 
         if num == square:
@@ -25,8 +24,33 @@ def find_square_root(num):
             return i-1
 
 num = 36
-num = 28
-num = 17
+# num = 28
+# num = 10
+num = 1
 print(find_square_root(num))
 
+def find_square_root(num):
 
+    low = 1
+    high = num
+    ans = 0
+
+    if num <=1:
+        return num
+
+    while low <= high:
+
+        mid = (low+high)//2
+
+        if mid*mid == num:
+            return mid
+        elif mid*mid > num:
+            high = mid-1
+        else:
+            ans = mid
+            low = mid+1
+
+    return  ans
+
+
+print(find_square_root(num))
