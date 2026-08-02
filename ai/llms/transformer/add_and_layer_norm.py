e6fdf24fd8539fc114dadcae2fa0cd01fa1f107d
machinoAI <ravikk87@gmail.com>
2026-08-02 16:49:56 +0530

    add & LayerNorm

--- ai/llms/transformer/add_and_layer_norm.py
+++ ai/llms/transformer/add_and_layer_norm.py
@@ -51,6 +51,44 @@
     - Because the input skips the computation and is added later.
     - The input bypasses the attention layer.
 
+4. What is LayerNorm ?
+
+    - Layer Normalization stablizes the values flowing through the network
+
+    - It rescales the features for each token so they have a controlled distribution.
+
+    For one token's hidden vector:
+        [2, 10, -5, 100] -->>  [-0.4, 0.1, -1.2, 1.5]
+
+5. Why do we need LayerNorm ?
+
+    - Without LayerNorm , hidden values can grow or shrink unpredictably extremely large/ small across many layers.
+
+
+6. How LayerNorm works ?
+    - For each token independently:
+        Hidden State
+
+            [3, 5, 7, 9]
+
+    - Compute the mean:
+        Mean = 6
+
+    - Subtract it:
+        [-3, -1, 1, 3]
+
+    - Compute the standard deviation and divide by it:
+        [-1.34, -0.45, 0.45, 1.34]
+
+
+    - Then apply two learnable parameters:
+        Output = γ × normalized + β
+
+        where:
+            γ learns the scale.
+            β learns the shift.
+
+            - These are trained with the rest of the model.
 
 
 
