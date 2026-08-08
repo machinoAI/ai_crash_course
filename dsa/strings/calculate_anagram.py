9ac07f7812406735c77c8cd941be224e1194592f
machinoAI <ravikk87@gmail.com>
2026-08-08 17:53:51 +0530

    calculate anagram

--- dsa/strings/calculate_anagram.py
+++ dsa/strings/calculate_anagram.py
@@ -21,3 +21,34 @@ def calculate_anagram(words):
 
 
 print(calculate_anagram(words))
+
+
+# Using Defaultdict
+
+from collections import defaultdict
+
+groups = defaultdict(list)
+
+for word in words:
+    key = ''.join(sorted(word))
+    groups[key].append(word)
+
+print(list(groups.values()))
+
+
+
+# You can avoid sorting by using a 26-character frequency tuple as the key:
+
+from collections import defaultdict
+
+groups = defaultdict(list)
+
+for word in words:
+    count = [0] * 26
+
+    for ch in word:
+        count[ord(ch) - ord('a')] += 1
+
+    groups[tuple(count)].append(word)
+
+print(list(groups.values()))
