b8b4b7bb5daee76e4d0f8c5176317e936171edea
machinoAI <ravikk87@gmail.com>
2026-08-11 16:02:51 +0530

    When not to choose the AI/LLM

---
+++ ml_system_design/when_not_to_choose_llm.py
@@ -0,0 +1,12 @@
+"""
+1. In which scenario you should not use AI/LLMs in your projects and why ?
+
+-   LLM is a powerful tool that not means we should use it everywhere, I can think of 5 scenarios
+    where I believe we should not be using LLM.
+
+    1. Simple and deterministic rule is enough.
+    2. LLM Cost is higher than the expected business value:
+    3. Accuracy and consistency are the critical: Financial, Healthcare fields, Taxation etc.
+    4. Privacy and compliance concerns.
+    5. Human Judgement is important: Negotiations, Strategy, Reviewing etc.
+"""
