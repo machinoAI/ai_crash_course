5a772033565f45b3e141ffcde48374c9e248f6fc
machinoAI <ravikk87@gmail.com>
2026-08-23 17:37:03 +0530

    Database design

---
+++ system_design_hld/database_design.py
@@ -0,0 +1,19 @@
+"""
+Database Design Pattern:
+
+    1. Requirement clarification
+    2. Understanding Business workflow
+    3. Identify Entities
+    4. Identify queries
+    5. Design Schema
+    6. Relationship and Constraints
+    7. Indexing based on query retrieval
+    8. Discussing of Edge cases
+    9. Architecture Choices: ACID, SAGA, CAP Theorem
+    10. Discussing NFRs: Scaling, Caching, Observability,
+
+
+
+
+
+"""
