ab1a6d64d27389d5337b5f6e406acb689b94477b
machinoAI <ravikk87@gmail.com>
2026-08-25 09:56:25 +0530

    merge sort

--- dsa/arrays/sorting.py
+++ dsa/arrays/sorting.py
@@ -3,6 +3,8 @@ Sort the given list
 nums = [8,3,55,33,1,99,833,0,-1]
 output=[-1,0,1,3,8,33,55,99,833]
 """
+
+
 # Bubble sort: Repeatedly compare adjacent elements and swap them if they are in the wrong order.
 # TC: O(n^2)
 # SC: O(1)
@@ -59,3 +61,34 @@ print("Selection Sort: ", sort_numbers(nums))
 # TC: O(n log n)
 # SC: O(n)
 
+
+def merge_sort(nums):
+
+    if len(nums) == 1:
+        return nums
+
+    mid = len(nums) // 2
+
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+
+        if left[i] < right[j]:
+            result.append(left[i])
+            i +=1
+
+        else:
+            result.append(right[j])
+            j +=1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+
+print("Merge Sort: ", merge_sort(nums))
