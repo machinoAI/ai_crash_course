0cf6e6e3023118b5e2d404f4d390512e427936e2
machinoAI <ravikk87@gmail.com>
2026-08-27 00:10:26 +0530

    find missing number

--- dsa/arrays/find_missing_number.py
+++ dsa/arrays/find_missing_number.py
@@ -14,3 +14,23 @@ def find_missing_number(nums):
     return len(nums)
 
 print(find_missing_number(nums))
+
+
+def find_missing_number(arr):
+    n = len(arr) + 1
+
+    xor_all = 0
+    xor_arr = 0
+
+    for i in range(1, n + 1):
+        xor_all ^= i
+
+    for num in arr:
+        xor_arr ^= num
+
+    return xor_all ^ xor_arr
+
+
+arr = [8, 2, 4, 5, 3, 7, 1]
+
+print(find_missing_number(arr))
