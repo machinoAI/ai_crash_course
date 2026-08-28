607acac4683df398f28fc87f288abf3d60134c02
machinoAI <ravikk87@gmail.com>
2026-08-28 19:04:42 +0530

    fixed

--- dsa/arrays/sorting_frequency_based.py
+++ dsa/arrays/sorting_frequency_based.py
@@ -1,9 +1,16 @@
+"""
+nums = [ 4, 5, 6, 5,4,3]
+
+output = [6, 3 , 5, 5 , 4, 4]
+
+"""
+
 from collections import Counter
 
 def sort_by_frequency(nums):
     freq = Counter(nums)
 
-    return sorted(nums, key=lambda x: freq[x])
+    return sorted(nums, key=lambda x: (freq[x], -x))
 
 
 nums = [4, 5, 6, 5, 4, 3]
