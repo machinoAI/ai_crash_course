b5c59b148f44078ce7d522aeffa98ad527d92c1c
machinoAI <ravikk87@gmail.com>
2026-08-29 16:29:52 +0530

    Tensors

---
+++ deep_learning/pytorch/tensors.py
@@ -0,0 +1,35 @@
+"""
+1. Tensors: Tensor is specialized multi-dimensional array designed for mathematical and computational efficiency.
+
+    Real world Examples:
+        1. Scalars: 0-dimensional tensors ( A single number)
+            - Loss value : 5.0, 3.7
+
+        2. Vectors: 1-dimensional tensors
+            - Feature vectors
+            - Embeddings [0.2, -04, 0.8, 0.5]
+
+        3. Matrices: 2-dimensional tensors
+
+            - Tabular or grid data
+            - Grayscale images
+            - [
+                [ 0, 255, 128],
+                [34, 90, 180]
+              ]
+
+        4. 3D tensors: Coloured Images
+            - Adds a third dimension, often used for stacking data
+            - RGB Images: [Width x height x Channels]
+
+        5. 4D Tensors: Batches of RGB
+            - Batches of RGB images
+            - [ Batch size x Width x height x Channels]
+
+        6. 5D Tensors: Add a time dimension for data that changes over time.
+
+            - Video Clips
+
+
+
+"""
